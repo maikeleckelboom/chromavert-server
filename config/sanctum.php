d210bb37bc7e9c7845ddeb23e6c2e6f0e62c7494
Maikel Eckelboom <57023072+maikeleckelboom@users.noreply.github.com>
2024-04-27 01:05:42 +0200

    chore: try things quick

--- config/sanctum.php
+++ config/sanctum.php
@@ -1,6 +1,5 @@
 <?php
 
-use App\Http\Middleware\AuthenticateSession;
 use Laravel\Sanctum\Sanctum;
 
 return [
@@ -18,7 +17,7 @@ return [
 
     'stateful' => explode(',', env('SANCTUM_STATEFUL_DOMAINS', sprintf(
         '%s%s%s',
-        '*.chromavert.com,*.api.chromavert.com,localhost,localhost:3000,127.0.0.1,127.0.0.1:8000,::1',
+        'chromavert.com,api.chromavert.com,localhost,localhost:3000,127.0.0.1,127.0.0.1:8000,::1',
         Sanctum::currentApplicationUrlWithPort(),
         env('FRONTEND_URL') ? ','.parse_url(env('FRONTEND_URL'), PHP_URL_HOST) : ''
     ))),
@@ -77,7 +76,7 @@ return [
     */
 
     'middleware' => [
-        'authenticate_session' => AuthenticateSession::class,
+        'authenticate_session' => Laravel\Sanctum\Http\Middleware\AuthenticateSession::class,
         'encrypt_cookies' => Illuminate\Cookie\Middleware\EncryptCookies::class,
         'validate_csrf_token' => Illuminate\Foundation\Http\Middleware\ValidateCsrfToken::class,
     ],
--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,27 @@ use App\Http\Controllers\User\CurrentUserController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return ['Laravel' => app()->version()];
+    return [
+        'Laravel' => app()->version(),
+        'PHP' => PHP_VERSION,
+        'Framework' => 'Laravel',
+        'Status' => 'OK',
+        'State' => [
+            'Environment' => config('app.env'),
+            'Debug' => config('app.debug') ? 'Enabled' : 'Disabled',
+            'URL' => config('app.url'),
+            'Timezone' => config('app.timezone'),
+            'Locale' => config('app.locale'),
+            'Database' => config('database.default'),
+            'Cache' => config('cache.default'),
+            'Queue' => config('queue.default'),
+            'Session' => config('session.driver'),
+        ],
+        'User' => [
+            'Authenticated' => auth()->check() ? 'Yes' : 'No',
+            'User' => auth()->check() ? auth()->user() : null,
+        ]
+    ];
 });
 
 require __DIR__ . '/auth.php';
