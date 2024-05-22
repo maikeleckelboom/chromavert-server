4d35c0f094b47d31eec7c12aadd914b06e554198
Maikel Eckelboom <57023072+maikeleckelboom@users.noreply.github.com>
2024-05-22 04:23:35 +0200

    improve test with redirect assert

--- tests/Feature/Auth/IdentityProviderTest.php
+++ tests/Feature/Auth/IdentityProviderTest.php
@@ -2,8 +2,7 @@
 
 use Laravel\Socialite\Facades\Socialite;
 
-
-test('can authenticate using github provider', function () {
+test('can authenticate using socialite provider', function () {
     $abstractUser = Mockery::mock('Laravel\Socialite\Two\User');
     $abstractUser
         ->shouldReceive('getId')
@@ -20,25 +19,5 @@ test('can authenticate using github provider', function () {
     $provider->shouldReceive('user')->andReturn($abstractUser);
     Socialite::shouldReceive('driver')->with('github')->andReturn($provider);
     $response = $this->get('/auth/github/callback');
-    $response->assertRedirect();
-});
-
-test('can authenticate using google provider', function () {
-    $abstractUser = Mockery::mock('Laravel\Socialite\Two\User');
-    $abstractUser
-        ->shouldReceive('getId')
-        ->andReturn(1234567890)
-        ->shouldReceive('getEmail')
-        ->andReturn('socialte@example.com')
-        ->shouldReceive('getNickname')
-        ->andReturn('socialite')
-        ->shouldReceive('getName')
-        ->andReturn('Socialite User')
-        ->shouldReceive('getAvatar')
-        ->andReturn('https://en.gravatar.com/userimage');
-    $provider = Mockery::mock('Laravel\Socialite\Contracts\Provider');
-    $provider->shouldReceive('user')->andReturn($abstractUser);
-    Socialite::shouldReceive('driver')->with('google')->andReturn($provider);
-    $response = $this->get('/auth/google/callback');
-    $response->assertRedirect();
+    $response->assertRedirect(config('app.frontend_url').'/account');
 });
