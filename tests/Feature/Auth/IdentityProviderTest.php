c6402174fdc06453062fe4ad807dcf2aa2d15979
Maikel Eckelboom <57023072+maikeleckelboom@users.noreply.github.com>
2024-05-22 03:49:53 +0200

    fix: tests

--- tests/Feature/Auth/IdentityProviderTest.php
+++ tests/Feature/Auth/IdentityProviderTest.php
@@ -1,35 +1,12 @@
 <?php
 
-use Laravel\Socialite\Contracts\Provider;
 use Laravel\Socialite\Facades\Socialite;
-use Laravel\Socialite\Two\User;
-use Mockery\MockInterface;
-
-test('can authenticate using socialite github provider', function () {
-    $providerMock = getProviderMock();
-    $providerUserMock = getProviderUserMock();
-    $providerMock->shouldReceive('user')->andReturn($providerUserMock);
-
-    Socialite::shouldReceive('driver')->with('github')->andReturn($providerMock);
-
-    $response = $this->get('/auth/github/callback');
-    $response->assertRedirect();
-});
-
-test('can authenticate using socialite google provider', function () {
-    $provider = getProviderMock();
-    $provider->shouldReceive('user')->andReturn(getProviderUserMock());
-
-    Socialite::shouldReceive('driver')->with('google')->andReturn($provider);
-    $response = $this->get('/auth/google/callback');
-    $response->assertRedirect();
-});
 
 
-function getProviderUserMock(): User&MockInterface
-{
-    $providerUser = Mockery::mock('Laravel\Socialite\Two\User');
-    return $providerUser->shouldReceive('getId')
+test('can authenticate using github provider', function () {
+    $abstractUser = Mockery::mock('Laravel\Socialite\Two\User');
+    $abstractUser
+        ->shouldReceive('getId')
         ->andReturn(1234567890)
         ->shouldReceive('getEmail')
         ->andReturn('socialte@example.com')
@@ -39,9 +16,29 @@ function getProviderUserMock(): User&MockInterface
         ->andReturn('Socialite User')
         ->shouldReceive('getAvatar')
         ->andReturn('https://en.gravatar.com/userimage');
-}
+    $provider = Mockery::mock('Laravel\Socialite\Contracts\Provider');
+    $provider->shouldReceive('user')->andReturn($abstractUser);
+    Socialite::shouldReceive('driver')->with('github')->andReturn($provider);
+    $response = $this->get('/auth/github/callback');
+    $response->assertRedirect();
+});
 
-function getProviderMock(): Provider&MockInterface
-{
-    return Mockery::mock('Laravel\Socialite\Contracts\Provider');
-}
+test('can authenticate using google provider', function () {
+    $abstractUser = Mockery::mock('Laravel\Socialite\Two\User');
+    $abstractUser
+        ->shouldReceive('getId')
+        ->andReturn(1234567890)
+        ->shouldReceive('getEmail')
+        ->andReturn('socialte@example.com')
+        ->shouldReceive('getNickname')
+        ->andReturn('socialite')
+        ->shouldReceive('getName')
+        ->andReturn('Socialite User')
+        ->shouldReceive('getAvatar')
+        ->andReturn('https://en.gravatar.com/userimage');
+    $provider = Mockery::mock('Laravel\Socialite\Contracts\Provider');
+    $provider->shouldReceive('user')->andReturn($abstractUser);
+    Socialite::shouldReceive('driver')->with('google')->andReturn($provider);
+    $response = $this->get('/auth/google/callback');
+    $response->assertRedirect();
+});
