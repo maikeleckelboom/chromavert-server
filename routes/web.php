4ec559ee06a867be1260e24f20bf644cff9ec50e
Maikel Eckelboom <57023072+maikeleckelboom@users.noreply.github.com>
2024-06-09 16:19:11 +0200

    fix: minor rebase issue

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use App\Http\Controllers\Auth\UpdatePasswordController;
+use App\Http\Controllers\Github\RepoContentController;
+use App\Http\Controllers\Github\RepoController;
 use App\Http\Controllers\User\IdentityProviderController;
 use App\Http\Controllers\User\ProfileController;
 use App\Http\Controllers\User\SessionController;
@@ -23,6 +25,12 @@ Route::group(['middleware' => 'auth:sanctum'], function () {
     Route::get('/user/sessions', [SessionController::class, 'index']);
     Route::delete('/user/sessions/{id}', [SessionController::class, 'destroy']);
     Route::delete('/user/other-sessions', [SessionController::class, 'destroyOtherSessions']);
+
+    Route::get('user/github/repositories', [RepoController::class, 'index']);
+    Route::get('user/github/{repo}', [RepoController::class, 'show']);
+    Route::get('user/github/{repo}/contents', [RepoContentController::class, 'index']);
+    Route::get('user/github/{repo}/contents/{path}', [RepoContentController::class, 'show']);
+    Route::get('user/github/{repo}/branches', [RepoContentController::class, 'branches']);
 });
 
 
